21c08714bf4ce3cf953aea30d8cc4d36a3f9e7f0
chaliman12 <chalchisatamiru10@gmail.com>
2023-08-12 10:02:44 +0300

    new commit

--- Final_web_Scrape.py
+++ Final_web_Scrape.py
@@ -2,47 +2,40 @@ import requests
 from bs4 import BeautifulSoup
 import pandas as pd
 import os
+topics_url = 'https://github.com/topics'
 base_url = 'https://github.com'
-
 def get_topics_page():
-    topics_url = 'https://github.com/topics'
     response = requests.get(topics_url)
     if response.status_code != 200:
         raise Exception('Failed to load page {}'.format(topics_url))
     doc = BeautifulSoup(response.text, 'html.parser')
     return doc
-
-
 def get_topic_titles(doc):
-    selection_class = 'f3 lh-condensed mb-0 mt-1 Link--primary'
-    topic_title_tags = doc.find_all('p', {'class': selection_class})
+    title_class = 'f3 lh-condensed mb-0 mt-1 Link--primary'
+    topic_title_tags = doc.find_all('p', {'class': title_class})
     topic_titles = []
     for tag in topic_title_tags:
         topic_titles.append(tag.text)
     return topic_titles
-
 def get_topic_descs(doc):
-    desc_selector = 'f5 color-fg-muted mb-0 mt-1'
-    topic_desc_tags = doc.find_all('p', {'class': desc_selector})
+    desc_class = 'f5 color-fg-muted mb-0 mt-1'
+    topic_desc_tags = doc.find_all('p', {'class': desc_class})
     topic_descs = []
     for tag in topic_desc_tags:
         topic_descs.append(tag.text.strip())
     return topic_descs
-
 def get_topic_urls(doc):
-    topic_link_tags = doc.find_all('a', {'class': 'no-underline flex-1 d-flex flex-column'})
+    url_class='no-underline flex-1 d-flex flex-column'
+    topic_link_tags = doc.find_all('a', {'class':url_class })
     topic_urls = []
     base_url = 'https://github.com'
     for tag in topic_link_tags:
         topic_urls.append(base_url + tag['href'])
     return topic_urls
-
-
 def scrape_topics():
-    topics_url = 'https://github.com/topics'
     response = requests.get(topics_url)
     if response.status_code != 200:
-        raise Exception('Failed to load page {}'.format(topic_url))
+        raise Exception('Failed to load page {}'.format(topics_url))
     doc = BeautifulSoup(response.text, 'html.parser')
     topics_dict = {
         'title': get_topic_titles(doc),
@@ -50,10 +43,6 @@ def scrape_topics():
         'url': get_topic_urls(doc)
     }
     return pd.DataFrame(topics_dict)
-
-# print(scrape_topics())
-
-
 def get_topic_page(topic_url):
     # Download the page
     response = requests.get(topic_url)
@@ -63,13 +52,11 @@ def get_topic_page(topic_url):
     # Parse using Beautiful soup
     topic_doc = BeautifulSoup(response.text, 'html.parser')
     return topic_doc
-
 def parse_star_count(stars):
     stars=stars.strip()
     if stars[-1]=='k':
         return int(float(stars[:-1])*1000)
     return(int(stars))
-
 def get_repo_info(h1_tag, star_tag):
     # returns all the required info about a repository
     a_tags = h1_tag.find_all('a')
@@ -78,13 +65,14 @@ def get_repo_info(h1_tag, star_tag):
     repo_url =  base_url + a_tags[1]['href']
     stars = parse_star_count(star_tag.text.strip())
     return username, repo_name, stars, repo_url
-
 def get_topic_repos(topic_doc):
     # Get the h1 tags containing repo title, repo URL and username
-    repo_tags = topic_doc.find_all('article',{'class':'border rounded color-shadow-small color-bg-subtle my-4'})
+    repo_class='border rounded color-shadow-small color-bg-subtle my-4'
+    repo_tags = topic_doc.find_all('article',{'class':repo_class})
 
     # Get star tags
-    star_tags=topic_doc.find_all('span',{'id':'repo-stars-counter-star'})
+    star_class='repo-stars-counter-star'
+    star_tags=topic_doc.find_all('span',{'id':star_class})
     
     topic_repos_dict = { 'username': [], 'repo_name': [], 'stars': [],'repo_url': []}
 
@@ -97,15 +85,12 @@ def get_topic_repos(topic_doc):
         topic_repos_dict['repo_url'].append(repo_info[3])
         
     return pd.DataFrame(topic_repos_dict)
-
 def scrape_topic(topic_url, path):
     if os.path.exists(path):
         print("The file {} already exists. Skipping...".format(path))
         return
     topic_df = get_topic_repos(get_topic_page(topic_url))
-    topic_df.to_csv(path, index=None)
-    
-    
+    topic_df.to_csv(path, index=None)  
 def scrape_topics_repos():
     print('Scraping list of topics')
     topics_df = scrape_topics()
@@ -114,4 +99,4 @@ def scrape_topics_repos():
     for index, row in topics_df.iterrows():
         print('Scraping top repositories for "{}"'.format(row['title']))
         scrape_topic(row['url'], 'data/{}.csv'.format(row['title']))
-print(scrape_topics_repos())
+scrape_topics_repos()
--- bbc.py
+++ bbc.py
@@ -1,56 +1,53 @@
 import requests
 from bs4 import BeautifulSoup
 import pandas as pd
-import os
 
-def get_topics_page():
-    topics_url = 'https://www.bbc.com/afaanoromoo'
-    response = requests.get(topics_url)
+def get_news_page():
+    news_url = 'https://www.bbc.com/afaanoromoo'
+    response = requests.get(news_url)
     if response.status_code != 200:
-        raise Exception('Failed to load page {}'.format(topics_url))
+        raise Exception('Failed to load page {}'.format(news_url))
     doc = BeautifulSoup(response.text, 'html.parser')
     return doc
-def get_topic_titles(doc):
+def get_news_titles(doc):
     selection_class = 'focusIndicatorDisplayInlineBlock bbc-1mirykb ecljyjm0'
-    topic_title_tags = doc.find_all('a', {'class': selection_class})
-    topic_titles = []
-    for tag in topic_title_tags:
-        topic_titles.append(tag.text)
-    return topic_titles
-def get_topic_descs(doc):
+    news_title_tags = doc.find_all('a', {'class': selection_class})
+    news_titles = []
+    for tag in news_title_tags:
+        news_titles.append(tag.text)
+    return news_titles
+def get_news_descs(doc):
     desc_selector = 'bbc-idwms3 e1mklfmt0'
-    topic_desc_tags = doc.find_all('time', {'class': desc_selector})
-    topic_descs = []
-    for tag in topic_desc_tags:
-        topic_descs.append(tag.text.strip())
-    return topic_descs
-desc=get_topic_descs(get_topics_page())
-
-def get_topic_urls(doc):
-    topic_link_tags = doc.find_all('a', {'class': 'focusIndicatorDisplayInlineBlock bbc-1mirykb ecljyjm0'})
-    topic_urls = []
+    news_desc_tags = doc.find_all('time', {'class': desc_selector})
+    news_descs = []
+    for tag in news_desc_tags:
+        news_descs.append(tag.text.strip())
+    return news_descs
+def get_news_urls(doc):
+    url_class= 'focusIndicatorDisplayInlineBlock bbc-1mirykb ecljyjm0'
+    topic_link_tags = doc.find_all('a', {'class':url_class})
+   
+    news_urls = []
     base_url = 'https://www.bbc.com/'
     for tag in topic_link_tags:
-        topic_urls.append(base_url + tag['href'])
-    return topic_urls
-
-
-def scrape_topics():
-    topics_url = 'https://www.bbc.com/afaanoromoo'
-    response = requests.get(topics_url)
+        news_urls.append(base_url + tag['href'])
+    return news_urls
+def scrape_news():
+    news_url = 'https://www.bbc.com/afaanoromoo'
+    response = requests.get(news_url)
     if response.status_code != 200:
-        raise Exception('Failed to load page {}'.format(topic_url))
+        raise Exception('Failed to load page {}'.format(news_url))
     doc = BeautifulSoup(response.text, 'html.parser')
-    topics_dict = {
-        'title': get_topic_titles(doc),
-        'description': get_topic_descs(doc),
-        'url': get_topic_urls(doc)
+    news_dict = {
+        'Mata_duree': get_news_titles(doc),
+        'Turtii_yeroo': get_news_descs(doc),
+        'Liinkii': get_news_urls(doc)
     }
-    topic_df=pd.DataFrame(topics_dict)
-    return topic_df.to_csv('bbc.csv',index=None)
-scrape_topics()   
+    news_df=pd.DataFrame(news_dict)
+    print("Scraping for BBC Afaan Oromoo...") 
+    return news_df.to_csv('Oduu_Ammee.csv',index=None)
+scrape_news()   
 
-  
 
 
 
